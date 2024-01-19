0dcee1aecc908d21b64d997d76aebe9da031b03e
upqnu <upgrdrqnu@gmail.com>
2024-01-19 13:52:06 +0900

    init commit

---
+++ src/test/java/upqnu/jobpost/JobpostApplicationTests.java
@@ -0,0 +1,13 @@
+package upqnu.jobpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobpostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
